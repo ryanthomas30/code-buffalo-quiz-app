1c11ab493cb919fa008e792a8f4e98d6fc6524a9
Liam Gensel <liamgens@gmail.com>
2020-04-13 21:54:54 -0400

    allow all hosts cors

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 import schemas
@@ -12,6 +13,14 @@ from database import session, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/quizzes', response_model=List[schemas.QuizResponse])
