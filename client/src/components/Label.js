e1cab240b24301f3b2d778f43f093bf5e59a0013
Liam Gensel <liamgens@gmail.com>
2020-04-17 00:15:12 -0400

    remove client

--- client/src/components/Label.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Label = styled.label`
-	color: ${({ color }) => color ? color : '#827F7F'};
-	font-size: ${({ fontSize }) => fontSize || 'inherit'};
-	font-weight: ${({ fontWeight }) => fontWeight || 'inherit'};
-`
--- client/src/components/Link.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import { Link as BaseLink } from 'react-router-dom'
-
-export const Link = styled(BaseLink)`
-	color: unset;
-	text-decoration: none;
-`
