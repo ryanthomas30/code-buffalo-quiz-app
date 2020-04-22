aaca8a444e228a009ff18fc692e197596955c812
Liam Gensel <liamgens@gmail.com>
2020-04-22 16:56:01 -0400

    update database URL

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://quiz_app_user:password@localhost:5432/quiz_app')
+engine = create_engine(os.getenv('DATABASE_URL'))
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
