e0613334c83a626519853f79d0f5d1ff9464eaf5
Liam Gensel <liamgens@gmail.com>
2020-04-22 17:00:02 -0400

    import os

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
 
 engine = create_engine(os.getenv('DATABASE_URL'))
 Session = sessionmaker(bind=engine)
